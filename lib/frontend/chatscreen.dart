04db7829744a9b56ae6c413521318445aacb0133
Havockeyan <karthihavoc121@gmail.com>
2021-05-11 19:18:50 +0530

    structure

---
+++ lib/frontend/chatscreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class chatscreen extends StatelessWidget {
+  const chatscreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
