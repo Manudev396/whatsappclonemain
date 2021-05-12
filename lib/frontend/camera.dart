0d766a94062841b208bb84d0dde0ea25fa7b9dfd
Havockeyan <karthihavoc121@gmail.com>
2021-05-12 17:47:40 +0530

    small change

--- lib/frontend/camera.dart
+++ lib/frontend/camera.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'package:flutter_flashlight/flutter_flashlight.dart';
+
 
 
 class Camera extends StatefulWidget {
